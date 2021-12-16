33ccdeaee50be822e7df85573c5949a172ddb09c
sdinanozer <inanozer41@hotmail.com>
2021-12-17 00:10:41 +0300

    added contact list

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 //import * as React from 'react';
 //import { AppRegistry } from 'react-native';
-import { StyleSheet, FlatList, View, KeyboardAvoidingView } from 'react-native';
+import { StyleSheet, FlatList, View, KeyboardAvoidingView, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Appbar, TextInput, FAB, Button, Paragraph, Text, Headline, Provider as PaperProvider, Title, Card } from 'react-native-paper';
+import { Appbar, TextInput, FAB, Button, Paragraph, Text, List, 
+         Headline, Provider as PaperProvider, Title, Card } from 'react-native-paper';
 //import { expo as appName } from './app.json';
 
 const Stack = createNativeStackNavigator();
@@ -62,6 +63,45 @@ const ContactScreen = ({ navigation }) => {
         <Appbar.Action style={{alignItems: 'flex-end', flex:1}} icon="magnify" onPress={() => console.log("Pressed search.")} />
         <Appbar.Action icon="filter-variant" onPress={() => console.log("Pressed filter.")} />
       </Appbar>
+
+      <View style={styles.contactContainer}>
+        <FlatList
+          data={[
+            {id: 1, name: 'Vincent', pos: 'ceo'},
+            {id: 2, name: 'Summer', pos: 'ceo'},
+            {id: 3, name: 'Jane', pos: 'ceo'},
+            {id: 4, name: 'Birdie', pos: 'ceo'},
+            {id: 5, name: 'Devin', pos: 'ceo'},
+            {id: 6, name: 'Dan', pos: 'ceo'},
+            {id: 7, name: 'Dominic', pos: 'ceo'},
+            {id: 8, name: 'Jackson', pos: 'ceo'},
+            {id: 9, name: 'James', pos: 'ceo'},
+            {id: 10, name: 'Joel', pos: 'ceo'},
+            {id: 11, name: 'John', pos: 'ceo'},
+            {id: 12, name: 'Jillian', pos: 'ceo'},
+            {id: 13, name: 'Jimmy', pos: 'ceo'},
+            {id: 14, name: 'Julie', pos: 'ceo'},
+          ]}
+          renderItem={({item}) => <List.Item
+                                    title={item.name}
+                                    description={item.pos}
+                                    left={props => <List.Icon {...props} icon="folder" />}
+                                  />}
+          ItemSeparatorComponent={
+            //Platform.OS == 'android' &&
+            (({ highlighted }) => (
+              <View
+                style={{
+                  borderBottomColor: '#bbbbbb',
+                  marginHorizontal: 20,
+                  borderBottomWidth: 2,
+                }}
+              />
+            ))
+          }      
+        />
+      </View>
+
 
       <Appbar style={styles.barBottom}>
         <Appbar.Action icon="contacts" onPress={() => console.log("Pressed contacts.")} />
@@ -126,10 +166,28 @@ const styles = StyleSheet.create({
     right: 0,
     top: 0,
   },
+  contactContainer: {
+    flex: 1,
+    paddingTop: 24,
+    marginTop: 24,
+  },
+  contactItem: {
+    padding: 20,
+    marginVertical: 8,
+    fontSize: 18,
+    height: 44,
+  },
   menuText: {
     fontSize: 20,
     color: '#ffffff',
-    fontFamily: 'Roboto',
+    ...Platform.select({
+      ios: {
+        fontFamily: 'Papyrus',
+      },
+      android: {
+        fontFamily: 'Roboto',
+      }
+    }),
     fontWeight: 'bold',
   },
   fab: {
